2ccbaa9670f67aeb2430db4b4241c17d6687ab2b
Muhammad-Hamim <hamim.ml01@gmail.com>
2023-07-11 20:00:28 +0600

    make responsive

--- src/Pages/Home/AppDownloadSection.jsx
+++ src/Pages/Home/AppDownloadSection.jsx
@@ -5,7 +5,7 @@ import phone from "../../assets/Footer-img/image-4.png";
 const AppDownloadSection = () => {
   return (
     <div className="grid lg:grid-cols-2">
-      <div className="bg-[#EDEFFC] pt-16 pl-44">
+      <div className="bg-[#EDEFFC] pt-16 pb-32 px-8 lg:pl-44">
         <div className="space-y-4">
           <h2 className="text-4xl font-bold">Get the App</h2>
           <p className="text-lg font-normal">Buy and Sell Anything.</p>
@@ -13,13 +13,13 @@ const AppDownloadSection = () => {
             Posting for Sale is Completely free
           </p>
         </div>
-        <div className="flex gap-8 items-center mt-8">
+        <div className="flex flex-wrap gap-8 items-center mt-8">
           <img className="cursor-pointer" src={appStore} alt="" />
           <img className="cursor-pointer" src={playStore} alt="" />
         </div>
       </div>
-      <div className="bg-[#A7AFEA] w-full h-full pr-44">
-        <img src={phone} className="lg:w-[612px] -ml-32 -mt-28" alt="" />
+      <div className="bg-[#A7AFEA] w-full h-full lg:pr-44">
+        <img src={phone} className="lg:w-[512px] lg:-ml-32 -mt-28" alt="" />
       </div>
     </div>
   );
