b27ca8c54f8ca408b70f236838e87aa0e286cb44
hridoyfahad30 <hridoyfahad30@gmail.com>
2023-07-17 22:06:43 +0600

    Update products Card & data load dynamically

--- src/Layout/Home.jsx
+++ src/Layout/Home.jsx
@@ -1,9 +1,21 @@
+import { useEffect, useState } from "react";
 import ProductCard from "../Components/ProductCard/ProductCard";
 import AppDownloadSection from "../Pages/Home/AppDownloadSection";
 
 import Banner from "../Pages/Home/Banner";
 
 const Home = () => {
+
+  const [products, setProducts] = useState([""]);
+
+  useEffect(()=>{
+
+    fetch('/products.json')
+    .then(res => res.json())
+    .then(data => setProducts(data))
+
+  },[])
+
   return (
     <div>
       <Banner />
@@ -13,16 +25,9 @@ const Home = () => {
           <button className="text-blue-500 text-lg font-semibold hover:underline">See All</button>
         </div>
         <div className="my-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-5 gap-6">
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
+          {
+            products.map((product, i) => <ProductCard key={i} product={product} />)
+          }
         </div>
       </div>
       <div className="mt-[500px]">
